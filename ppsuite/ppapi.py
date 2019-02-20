7857ac32f22bd993ea1d37244937666b7b865906
Dawson Whitehead <dwahme1@gmail.com>
2019-02-20 12:29:59 -0600

    Added better slide transitions

--- ppsuite/ppapi.py
+++ ppsuite/ppapi.py
@@ -42,10 +42,16 @@ class PPAPI:
         except ValueError:
             return False
 
+    # Pulls up a slide
+    def show_slide(self, slide, time=.5):
+        self.pres.SlideShowWindow.View.GoToSlide(slide)
+        time.sleep(time)
+        return self.pres.Slides(slide)
+
     #init instr cache page
     def init_inst_cache(self):
         self.pres.Slides.Add(self.INSTR_CACHE, 12)
-        self.pres.SlideShowWindow.View.GoToSlide(self.INSTR_CACHE)
+        self.show_slide(self.INSTR_CACHE)
 
         slide = self.pres.Slides(self.INSTR_CACHE)
 
@@ -58,26 +64,24 @@ class PPAPI:
 
     #loads program from ppasm file
     def load_ppasm(self, file_path):
-        self.pres.SlideShowWindow.View.GoToSlide(self.INSTR_CACHE)
-        instr_cache_slide = self.pres.Slides(self.INSTR_CACHE)
+        slide = self.show_slide(self.INSTR_CACHE)
         shape_num = 2
 
         with open(file_path, 'r') as f:
             for asm_line in f:
-                if instr_cache_slide.Shapes.Count < shape_num:
-                    instr_cache_slide.Shapes.AddTextbox(Orientation=0x1,
+                if slide.Shapes.Count < shape_num:
+                    slide.Shapes.AddTextbox(Orientation=0x1,
                                                         Left=100,
                                                         Top=20 * shape_num,
                                                         Width=300,
                                                         Height=30)
                 print(asm_line.split('\t'))
-                instr_cache_slide.Shapes(shape_num).TextFrame.TextRange.Text = asm_line.split('\t')[2]
+                slide.Shapes(shape_num).TextFrame.TextRange.Text = asm_line.split('\t')[2]
                 shape_num += 1
 
     #returns next instr
     def get_next_instr(self):
-        self.pres.SlideShowWindow.View.GoToSlide(self.INSTR_CACHE)
-        time.sleep(.5)
+        self.show_slide(self.INSTR_CACHE)
 
         instr_cache_slide = self.pres.Slides(self.INSTR_CACHE)
         ip_num = int(instr_cache_slide.Shapes(1).TextFrame.TextRange.Text)
@@ -87,19 +91,14 @@ class PPAPI:
 
     #updates instruction counter
     def update_instr_ptr(self, new_num):
-        self.pres.SlideShowWindow.View.GoToSlide(self.INSTR_CACHE)
-        time.sleep(.5)
-
-        instr_cache_slide = self.pres.Slides(self.INSTR_CACHE)
-        instr_cache_slide.Shapes(1).TextFrame.TextRange.Text = str(int(new_num) + 1)
+        slide = self.show_slide(self.INSTR_CACHE)
+        slide.Shapes(1).TextFrame.TextRange.Text = str(int(new_num) + 1)
 
     # Initializes the register page
     def init_register(self):
         self.pres.Slides.Add(self.REG, 12)
 
-        slide = self.pres.Slides(self.REG)
-        self.pres.SlideShowWindow.View.GoToSlide(self.REG)
-        time.sleep(.25)
+        slide = self.show_slide(self.REG)
 
         for reg_num in range(1, 10):
             slide.Shapes.AddTextbox(Orientation=0x1,
@@ -115,28 +114,23 @@ class PPAPI:
                 textframe.TextRange.Text = "X{}: 0".format(reg_num - 1)
 
     # Writes a val to a register
-    def reg_write(self, reg_name, val):
+    def reg_write_raw(self, reg_name, val):
         print(reg_name)
         reg_num = self.reg_table[reg_name]
 
-        self.pres.SlideShowWindow.View.GoToSlide(self.REG)
-        time.sleep(.25)
-
-        slide = self.pres.Slides(self.REG)
+        slide = self.show_slide(self.REG)
         textframe = slide.Shapes(reg_num + 1).TextFrame
 
         textframe.TextRange.Text = "X{}: {}".format(reg_num,val)
 
     # Reads a val from a register
-    def reg_read(self, reg_name):
+    def reg_read_raw(self, reg_name):
         print(reg_name)
 
         reg_num = self.reg_table[reg_name]
 
-        self.pres.SlideShowWindow.View.GoToSlide(self.REG)
-        time.sleep(.25)
+        slide = self.show_slide(self.REG)
 
-        slide = self.pres.Slides(self.REG)
         textframe = slide.Shapes(reg_num + 1).TextFrame
         val = int(textframe.TextRange.Text[4:])
 
@@ -146,26 +140,24 @@ class PPAPI:
     def init_mem(self):
         self.pres.Slides.Add(self.MEM_0, 12)
         self.pres.Slides.Add(self.MEM_1, 12)
-        slide_0 = self.pres.Slides(self.MEM_0)
+        slide_0 = self.show_slide(self.MEM_0)
         slide_1 = self.pres.Slides(self.MEM_1)
-        self.pres.SlideShowWindow.View.GoToSlide(self.MEM_0)
-        time.sleep(.5)
 
         for reg_num in range(1, 129):
             x = 15 + ((reg_num - 1) % 16) * 42
             y = 40 + 57 * ((reg_num - 1) // 16)
 
             slide_0.Shapes.AddTextbox(Orientation=0x1,
-                                    Left=x,
-                                    Top=y,
-                                    Width=45,
-                                    Height=18)
+                                      Left=x,
+                                      Top=y,
+                                      Width=45,
+                                      Height=18)
 
             slide_1.Shapes.AddTextbox(Orientation=0x1,
-                                    Left=x,
-                                    Top=y,
-                                    Width=45,
-                                    Height=18)
+                                      Left=x,
+                                      Top=y,
+                                      Width=45,
+                                      Height=18)
 
             textframe_0 = slide_0.Shapes(reg_num).TextFrame
             textframe_0.TextRange.Font.Size = 10
@@ -175,49 +167,29 @@ class PPAPI:
             textframe_1.TextRange.Text = "{}: {}".format(hex(reg_num - 1 + 128), hex(0))
 
     # Writes a val to mem
-    def mem_write(self, mem_loc, val):
-        overflow = mem_loc // 128
-
-        if overflow == 0:
-            slide = self.MEM_0
-        elif overflow == 1:
-            slide = self.MEM_1
-        else:
-            slide = self.MEM_1
-
-        self.pres.SlideShowWindow.View.GoToSlide(slide)
-        time.sleep(.5)
-
+    def mem_write_raw(self, mem_loc, val):
         mem_loc_real = mem_loc
+        slide_num = self.MEM_0
 
         if mem_loc > 127:
             mem_loc_real = mem_loc - 128
+            slide_num = self.MEM_1
 
-        slide = self.pres.Slides(slide)
+        slide = self.show_slide(slide_num)
         textframe = slide.Shapes(mem_loc_real + 1).TextFrame
 
         textframe.TextRange.Text = "{}: {}".format(hex(mem_loc), hex(val))
 
     # Reads a val from mem
-    def mem_read(self, mem_loc):
-        overflow = mem_loc // 128
-
-        if overflow == 0:
-            slide = self.MEM_0
-        elif overflow == 1:
-            slide = self.MEM_1
-        else:
-            slide = self.MEM_1
-
-        self.pres.SlideShowWindow.View.GoToSlide(slide)
-        time.sleep(.5)
-
+    def mem_read_raw(self, mem_loc):
         mem_loc_real = mem_loc
-
+        slide_num = self.MEM_0
+        
         if mem_loc > 127:
             mem_loc_real = mem_loc - 128
+            slide_num = self.MEM_1
 
-        slide = self.pres.Slides(slide)
+        slide = self.show_slide(slide_num)
         textframe = slide.Shapes(mem_loc_real + 1).TextFrame
 
         strip_len = len(hex(mem_loc)) + 2
@@ -229,7 +201,7 @@ class PPAPI:
         return val
 
     def tape_write_raw(self, tape_loc, val):
-        self.pres.SlideShowWindow.View.GoToSlide(tape_loc)
+        self.show_slide(tape_loc)
 
         lst = list(val)
         args = ["C:/Program Files/AutoHotkey/AutoHotkeyU64.exe",
@@ -262,7 +234,8 @@ class PPAPI:
                                          "hotkey/toggle_exec.ahk", "1"])
         ahk_teardown.wait()
 
-    def teardown(self):
+    # Need to find the delete function for slides
+    def teardown(self):pres.SlideShowWindow.View.GoToSlide
         self.pres.Slides.Delete(1) # MEM_0
         self.pres.Slides.Delete(1) # MEM_1
         self.pres.Slides.Delete(1) # REG
